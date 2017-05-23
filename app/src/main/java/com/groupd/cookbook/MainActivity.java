53c1ce468b0680be71ac342f7525d9f5dffaf7d3
Tao Wu <wutao19940420@gmail.com>
2017-05-23 17:11:25 -0500

    test3

--- app/src/main/java/com/groupd/cookbook/MainActivity.java
+++ app/src/main/java/com/groupd/cookbook/MainActivity.java
@@ -9,11 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //fuck the life
-//fuck pansiyu
 
-rewtwer
-        //fuck alll
-//???
     }
 }
