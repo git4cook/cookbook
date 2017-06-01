4d6b577dbc1d07279c52a0bc630d4ee56a05c539
Glenn Jayasuriya <jayasugp@myumanitoba.ca>
2017-06-01 15:50:01 -0500

    Made initial menu system for listing and searching. Note there is an bug in android studio editable and inputtype = none is not working. I added some code below editable in the textview List Recipies that makes it not editable

--- app/src/main/java/com/groupd/cookbook/MainActivity.java
+++ app/src/main/java/com/groupd/cookbook/MainActivity.java
@@ -1,7 +1,7 @@
 package com.groupd.cookbook;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.list_and_search_menu);
 
     }
 }
