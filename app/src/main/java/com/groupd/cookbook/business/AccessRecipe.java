257e9de9d3f990adf76122e51c109ea97421ae69
yinyuehan <pans34@myumanitoba.ca>
2017-06-02 15:40:06 -0500

    accessRecipe

--- app/src/main/java/com/groupd/cookbook/business/AccessRecipe.java
+++ app/src/main/java/com/groupd/cookbook/business/AccessRecipe.java
@@ -13,19 +13,40 @@ import com.groupd.cookbook.persistence.DataAccessStub;
 
 public class AccessRecipe {
     private DataAccessStub dataAccess;
-    private List<Recipe> elements;
+    private List<Recipe> recipes;
     private Recipe recipe;
     private int currentRecipe;
+
     public AccessRecipe (){
         dataAccess = Services.getDataAccess(Main.dbName);
-        elements = null;
+        recipes = null;
         recipe = null;
         currentRecipe = 0;
 }
 
+    public String getRecipe(List<Recipe> recipes)
+    {
+        recipes.clear();
+        return dataAccess.getRecipeSequential(recipes);
+    }
+
      public Recipe getSequential(){
-
-
-
+        String result = null;
+         if(recipe==null) {
+             result = dataAccess.getRecipeSequential(recipes);
+             currentRecipe = 0;
+         }
+         if(currentRecipe < recipes.size())
+         {
+          recipe = recipes.get(currentRecipe);
+             currentRecipe++;
+         }
+         else
+         {
+             recipes = null;
+             recipe  = null;
+             currentRecipe = 0;
+         }
+            return recipe;
      }
 }
