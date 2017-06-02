e3842ec405b0422b114e087c6cbd0ddb7ce49c7f
taowu <wutao19940420@gmail.com>
2017-06-02 13:57:29 -0500

    change position of recipe class

--- app/src/main/java/com/groupd/cookbook/objects/Recipe.java
+++ app/src/main/java/com/groupd/cookbook/objects/Recipe.java
@@ -1,4 +1,4 @@
-package com.groupd.cookbook.application;
+package com.groupd.cookbook.objects;
 
 /**
  * Created by Junwei on 2017/6/2.
--- app/src/main/java/com/groupd/cookbook/objects/recipes.java
+++
@@ -1,9 +0,0 @@
-package com.groupd.cookbook.objects;
-
-/**
- * Created by wutao on 2017/6/2.
- */
-
-public class recipes {
-    //
-}
