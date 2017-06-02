587aa32d8129274e429b1a35eb9d57507ab54901
Glenn Jayasuriya <jayasugp@myumanitoba.ca>
2017-06-02 15:29:09 -0500

    Commiting to pull from master

--- app/src/main/java/com/groupd/cookbook/MainActivity.java
+++ app/src/main/java/com/groupd/cookbook/MainActivity.java
@@ -1,10 +1,13 @@
 package com.groupd.cookbook;
 
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     // trying to get siyu to get my code
 
@@ -12,5 +15,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.list_and_search_menu);
+
+        Button addButton = (Button)findViewById(R.id.addButtonID);
+        addButton.setOnClickListener(this);
+        //Button addButton = (Button)findViewById(R.
+
     }//dont delete this activity
+
+
+    @Override
+    public void onClick(View v) {
+        Intent i = new Intent(this, addNewRecipe.class);
+        startActivity(i);
+    }
+
+
 }
---
+++ app/src/main/java/com/groupd/cookbook/addNewRecipe.java
@@ -0,0 +1,37 @@
+package com.groupd.cookbook;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class addNewRecipe extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_recipie);
+
+        Button submitButton = (Button)findViewById(R.id.submitButton);
+        submitButton.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        EditText titleQuery = (EditText)findViewById(R.id.addTitleID);
+        EditText tagsQuery = (EditText)findViewById(R.id.addTagsID);
+        EditText stepsQuery = (EditText)findViewById(R.id.addStepsID);
+
+        String titleInput = titleQuery.getText().toString();
+        String tagsInput = tagsQuery.getText().toString();
+        String stepsInput = stepsQuery.getText().toString();
+
+        System.out.println("The title is: " + titleInput);
+        Log.i("info", "title = " + titleInput);
+        Log.i("info", "tags = " + tagsInput);
+        Log.i("info", "steps = " + stepsInput);
+    }
+}
