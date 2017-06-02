9073d1b98f9839641d10c298d4a565fa292daed1
Glenn Jayasuriya <jayasugp@myumanitoba.ca>
2017-06-01 23:03:39 -0500

    Studio is telling me to commit

---
+++ app/src/main/java/com/groupd/cookbook/Main.java
@@ -0,0 +1,17 @@
+package com.groupd.cookbook;
+
+/**
+ * Created by siyupan on 2017/5/31.
+ */
+
+public class Main {
+
+
+    public static void main(String[] args){
+
+
+
+
+    }//main
+
+}//Main
