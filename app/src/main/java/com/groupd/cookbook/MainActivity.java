6ba5bd710d9bb0923a0023d4abf7e126cea1519d
Glenn Jayasuriya <jayasugp@myumanitoba.ca>
2017-06-01 23:18:44 -0500

    Linked my layout file for the menu system.

--- app/src/main/java/com/groupd/cookbook/MainActivity.java
+++ app/src/main/java/com/groupd/cookbook/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.list_and_search_menu);
     }//dont delete this activity
 }
