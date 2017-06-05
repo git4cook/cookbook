7172fda390aa22aad114bb97890e59fa08153514
Tao Wu <wutao19940420@gmail.com>
2017-06-04 23:12:46 -0500

    test

--- app/src/main/java/com/groupd/cookbook/presentation/MainActivity.java
+++ app/src/main/java/com/groupd/cookbook/presentation/MainActivity.java
@@ -6,25 +6,67 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ArrayAdapter;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.groupd.cookbook.R;
 import com.groupd.cookbook.business.AccessRecipe;
 import com.groupd.cookbook.objects.Recipe;
+
 import java.util.ArrayList;
 
 
-public class MainActivity extends AppCompatActivity /*implements View.OnClickListener*/{
+public class MainActivity extends AppCompatActivity /*implements View.OnClickListener*/ {
 
     //
     private AccessRecipe AR;
     private ArrayList<Recipe> Rlist;
     private ArrayAdapter<Recipe> RADP;
     private int selectedCoursePosition = -1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.test_list /*list_and_search_menu*/);
+
+        AR = new AccessRecipe();
+        Rlist = new ArrayList<Recipe>();
+        String rlt = AR.getRecipe(Rlist);
+        if (rlt != null)
+        {
+            Messages.fatalError(this, rlt);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         /*Button addButton = (Button)findViewById(R.id.addButtonID);
         addButton.setOnClickListener(this);*/
@@ -33,7 +75,7 @@ public class MainActivity extends AppCompatActivity /*implements View.OnClickLis
     }
 
 
-    @Override
+
     public void onClick(View v) {
         Intent i = new Intent(this, addNewRecipe.class);
         startActivity(i);
