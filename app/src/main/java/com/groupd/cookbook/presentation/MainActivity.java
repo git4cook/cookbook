9b439a0d3b630f317da6f6798673b7f7448d8f17
Tao Wu <wutao19940420@gmail.com>
2017-06-05 00:29:08 -0500

    MainActivity modified not finish yet

--- app/src/main/java/com/groupd/cookbook/presentation/MainActivity.java
+++ app/src/main/java/com/groupd/cookbook/presentation/MainActivity.java
@@ -22,12 +22,19 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
 import com.groupd.cookbook.R;
 import com.groupd.cookbook.business.AccessRecipe;
 import com.groupd.cookbook.objects.Recipe;
-
+import com.groupd.cookbook.application.Main;
 import java.util.ArrayList;
+
 
 
 public class MainActivity extends AppCompatActivity /*implements View.OnClickListener*/ {
@@ -36,13 +43,13 @@ public class MainActivity extends AppCompatActivity /*implements View.OnClickLis
     private AccessRecipe AR;
     private ArrayList<Recipe> Rlist;
     private ArrayAdapter<Recipe> RADP;
-    private int selectedCoursePosition = -1;
+    private int RecyPosy = -1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.test_list /*list_and_search_menu*/);
-
+        Main.startUp();
         AR = new AccessRecipe();
         Rlist = new ArrayList<Recipe>();
         String rlt = AR.getRecipe(Rlist);
@@ -50,7 +57,28 @@ public class MainActivity extends AppCompatActivity /*implements View.OnClickLis
         {
             Messages.fatalError(this, rlt);
         }
+        else{
 
+            RADP = new ArrayAdapter<Recipe>(this, android.R.layout.simple_list_item_activated_1, android.R.id.text1, Rlist)
+            {
+                @Override
+                public View getView(int position, View convertView, ViewGroup parent) {
+                    View view = super.getView(position, convertView, parent);
+
+                    TextView text1 = (TextView) view.findViewById(android.R.id.text1);
+                    //TextView text2 = (TextView) view.findViewById(android.R.id.text2);
+
+                    text1.setText(Rlist.get(position).getName());
+                    //text2.setText(Rlist.get(position).getCourseName());
+
+                    return view;
+                }
+            };
+
+
+
+
+        }
 
 
 
--- app/src/main/java/com/groupd/cookbook/presentation/showRecipe.java
+++ app/src/main/java/com/groupd/cookbook/presentation/showRecipe.java
@@ -2,13 +2,40 @@ package com.groupd.cookbook.presentation;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ArrayAdapter;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
 import com.groupd.cookbook.R;
 import com.groupd.cookbook.objects.Recipe;
 import com.groupd.cookbook.business.AccessRecipe;
+import com.groupd.cookbook.objects.Recipe;
 
-
-public class showRecipe extends AppCompatActivity {
+public class showRecipe extends AppCompatActivity/* implements View.OnClickListener*/{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
