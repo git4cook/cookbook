31eec0d8f37b0cd2006c7d6bb80db30bd83597b9
yinyu <pans34@myumanitoba.ca>
2017-06-22 18:55:21 -0500

    edit complete

--- app/src/main/java/com/groupd/cookbook/business/AccessRecipe.java
+++ app/src/main/java/com/groupd/cookbook/business/AccessRecipe.java
@@ -75,12 +75,12 @@ public class AccessRecipe {
     {
         return dataAccess.insertRecipe(currentRecipe);
     }
-//
-//    public String updateRecipe(Recipe currentRecipe)
-//    {
-//        return dataAccess.updateRecipe(currentRecipe);
-//    }
-//
+
+    public String updateRecipe(Recipe currentRecipe)
+  {
+      return dataAccess.updateRecipe(currentRecipe);
+  }
+
     public String deleteRecipe(Recipe currentRecipe)
     {
         return dataAccess.deleteRecipe(currentRecipe);
