5a8a069eb622373fc749811c8d9aad176be9b4f7
yinyuehan <pans34@myumanitoba.ca>
2017-06-25 14:03:20 -0500

    unit test and interface

--- app/src/test/java/com/groupd/cookbook/business/AccessRecipeTest.java
+++ app/src/test/java/com/groupd/cookbook/business/AccessRecipeTest.java
@@ -3,20 +3,35 @@ package com.groupd.cookbook.business;
 /**
  * Created by yinyu on 2017/6/22.
  */
+import com.groupd.cookbook.objects.Recipe;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.List;
+
+import dalvik.annotation.TestTargetClass;
+
 import static junit.framework.Assert.*;
 import static junit.framework.Assert.assertEquals;
 
 public class AccessRecipeTest {
     private AccessRecipe test;
     private String recipeName;
-
-    @Before
+    private Recipe a ;
+    private Recipe b;
+    private List<Recipe> testRecipes;
+   @Before
     public void setUp(){
-         test = new AccessRecipe();
-        recipeName = "pancake";
+       test = new AccessRecipe();
+       assertNotNull(test);
+       testRecipes =null;
+       a = new Recipe("pancake");
+       b = new Recipe ("cake");
+       testRecipes.add(a);
+       testRecipes.add(b);
+       recipeName = "pancake";
     }
     @After
     public void tearDown(){
@@ -26,11 +41,55 @@ public class AccessRecipeTest {
     @Test
    public void testGetRValid(){
 
-       assertEquals("pancake",recipeName);
+        Recipe result = test.getR("pancake");
+       assertEquals("error message",a,result);
    }
-  @Test
-    public void testGetRNumber(){
+   @Test
+    public void testGetRecipeValid(){
+       String result = test.getRecipe(testRecipes);
+       assertEquals("error message","pancake",result);
+   }
 
-  }
+    @Test
+    public void testSearchNull (){
+        boolean result = test.search(null);
+        assertEquals("error message",false,result);
+    }
+    @Test
+    public void testSearchValid (){
+        boolean result = test.search(recipeName);
+        assertEquals("error message",true,result);
+    }
+    @Test
+    public void testInsertRecipeNull(){
+        String result = test.insertRecipe(null);
+        assertEquals("error message",null,result);
+    }
+    @Test
+    public void testInsertRecipeValid(){
+        String result = test.insertRecipe(a);
+        assertEquals("error message","pancake",result);
 
+    }
+    @Test
+    public void testUpdateRecipeNull(){
+        String result = test.updateRecipe(null);
+        assertEquals("error message",null,result);
+    }
+    @Test
+    public void testUpdateRecipeValid(){
+        String result = test.updateRecipe(b);
+        assertEquals("error message","cake",result);
+
+    }
+    @Test
+    public void testDeleteRecipeNull(){
+        String result = test.deleteRecipe(null);
+       assertEquals("error message",null,result);
+    }
+    @Test
+    public void testDeleteRecipeValid(){
+        String result = test.deleteRecipe(a);
+        assertEquals("error message","cake",result);
+    }
 }
