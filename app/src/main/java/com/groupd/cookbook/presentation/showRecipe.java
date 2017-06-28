475d20e0eeb88e72a00102595c9f60b2fc242d87
yinyu <pans34@myumanitoba.ca>
2017-06-28 17:38:32 -0500

    fix bug

--- app/src/main/java/com/groupd/cookbook/presentation/showRecipe.java
+++ app/src/main/java/com/groupd/cookbook/presentation/showRecipe.java
@@ -35,11 +35,12 @@ public class showRecipe extends AppCompatActivity {
         title = vRcy.getName();
 
 
+
         TextView rti = (TextView) findViewById(R.id.vTitle);
         TextView rta = (TextView) findViewById(R.id.vTags);
         TextView rct = (TextView) findViewById(R.id.vDes);
         rti.setText(/*"Title: \n" + */title);
-        rta.setText("Tags: \n" + vRcy.getRecipeTags());
+        rta.setText("Tags: \n" + vRcy.tagToString());
         rct.setText("How to cook: \n" +  vRcy.getDirection());
 
     }
