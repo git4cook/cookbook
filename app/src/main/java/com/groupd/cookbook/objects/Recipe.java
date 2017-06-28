38d9321813e94bff2deb115942bc37f3a07c37de
yinyu <pans34@myumanitoba.ca>
2017-06-28 16:52:23 -0500

    fix bug

--- app/src/main/java/com/groupd/cookbook/objects/Recipe.java
+++ app/src/main/java/com/groupd/cookbook/objects/Recipe.java
@@ -50,8 +50,12 @@ public class Recipe implements RecipeInterface
     public String tagToString(){
         String tag ="";
         for(int i=0;i<tags.size();i++){
-            tag+=(tags.get(i).getTagsName())+",";
+            if(tag=="")
+                tag+=(tags.get(i).getTagsName());
+            else
+            tag+=","+(tags.get(i).getTagsName());
         }
+
         return tag;
     }
     /*public List<tag> getTagsList()
